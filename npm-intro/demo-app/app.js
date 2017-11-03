d72fbb1d6c652ce785c644017101b1a6165ee203
Mikael Eriksson <migluuu@gmail.com>
2017-11-03 20:18:55 +0200

    Delete app.js

--- npm-intro/demo-app/app.js
+++
@@ -1,9 +0,0 @@
-console.log("THIS IS APP.JS");
-
-// =================================
-// IMPORTS
-// =================================
-var cat = require("cat-me");
-var joke = require("knock-knock-jokes");
-
-console.log(joke());
